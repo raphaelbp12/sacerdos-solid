0cffa4f8db657de02029b2b6e6186d489cdfdc6d
Raphael Parreira <raphaelbp12@gmail.com>
2021-11-15 19:18:47 +0100

    feat: :sparkles: item attributes

--- Assets/Scripts/Items/EquipmentObject.cs
+++ Assets/Scripts/Items/EquipmentObject.cs
@@ -6,9 +6,6 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "New Equipment Object", menuName = "Inventory System/Items/Equipment")]
 public class EquipmentObject : ItemObject
 {
-    public float attackBonus;
-    public float defenseBonus;
-
     public void Awake()
     {
         type = ItemType.Equipment;
--- Assets/Scripts/Items/ItemObject.cs
+++ Assets/Scripts/Items/ItemObject.cs
@@ -9,6 +9,14 @@ public enum ItemType
         Default      
 }
 
+public enum Attributes
+{
+        Agility,
+        Inteligence,
+        Stamina,
+        Strength
+}
+
 public abstract class ItemObject : ScriptableObject
 {
         public int Id;
@@ -16,6 +24,13 @@ public abstract class ItemObject : ScriptableObject
         public ItemType type;
         [TextArea(15, 20)]
         public string description;
+        public ItemBuff[] buffs;
+
+        public Item CreateItem()
+        {
+                Item newItem = new Item(this);
+                return newItem;
+        }
 }
 
 [System.Serializable]
@@ -23,10 +38,41 @@ public class Item
 {
         public string Name;
         public int Id;
+        public ItemBuff[] buffs;
 
         public Item(ItemObject item)
         {
                 Name = item.name;
                 Id = item.Id;
+                buffs = new ItemBuff[item.buffs.Length];
+
+                for (int i = 0; i < buffs.Length; i++)
+                {
+                        buffs[i] = new ItemBuff(item.buffs[i].min, item.buffs[i].max)
+                        {
+                                attribute = item.buffs[i].attribute
+                        };
+                }
+        }
+}
+
+[System.Serializable]
+public class ItemBuff
+{
+        public Attributes attribute;
+        public int value;
+        public int min;
+        public int max;
+
+        public ItemBuff(int _min, int _max)
+        {
+                min = _min;
+                max = _max;
+                GenerateValue();
+        }
+
+        public void GenerateValue()
+        {
+                value = UnityEngine.Random.Range(min, max);
         }
 }
