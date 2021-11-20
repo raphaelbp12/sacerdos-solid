a283cafa4de2966afb5d87321faddb3563220158
Raphael Parreira <raphaelbp12@gmail.com>
2021-11-20 23:29:11 +0100

    feat: :sparkles: implement tooltip system

---
+++ Assets/Scripts/UI/TooltipManager.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TooltipManager : MonoBehaviour
+{
+    private static TooltipManager current;
+
+    public Tooltip tooltip;
+
+    private void Awake()
+    {
+        current = this;
+    }
+
+    public static void Show(string content, string header = "")
+    {
+        current.tooltip.SetText(content, header);
+        current.tooltip.gameObject.SetActive(true);
+    }
+
+    public static void Hide()
+    {
+        current.tooltip.gameObject.SetActive(false);
+    }
+}
